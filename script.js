e481b6a4a73e602ed611eeb49c3f815651b72fea
eva.wang <eva.wang81@gmail.com>
2022-04-02 16:27:40 +0800

    updated style sheet

--- script.js
+++ script.js
@@ -14,27 +14,37 @@ const buildBoard = (board) => {
   const boardContainer = document.createElement("div");
   boardContainer.className = "container";
   document.body.append(boardContainer);
-
   for (let i = 0; i < board.length; i += 1) {
     const boardRow = document.createElement("div");
-    boardRow.id = `row${i}`;
     boardRow.className = "row";
     boardContainer.append(boardRow);
     for (let j = 0; j < board[i].length; j += 1) {
       const boardElement = document.createElement("div");
       boardElement.className = "square";
-      // boardElement.innerText = `s${j + 1}`;
+      boardElement.addEventListener("click", () => {
+        boardElement.innerText = squareClicked(i, j);
+      });
+
       boardRow.append(boardElement);
     }
   }
 };
 
-const togglePlayer = (currentPlayer) => {
+const togglePlayer = () => {
   currentPlayer === "X" ? (currentPlayer = "O") : (currentPlayer = "X");
   return currentPlayer;
 };
 
-buildBoard(board);
-// Game Initialisation Logic
+const squareClicked = (row, column) => {
+  board[row][column] = currentPlayer;
+  togglePlayer();
+  console.log(currentPlayer);
+  return board[row][column];
+};
+// Initialize game
+const initGame = () => {
+  buildBoard(board);
+};
 
+initGame();
 //Gameplay Logic
